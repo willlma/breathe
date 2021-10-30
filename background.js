d8657b5c02cd4068438b3b0b714be475b992721e
Will Marquardt <wmarquardt@gmail.com>
2021-10-30 09:52:56 -0700

    main: don't see more than 25 items and make session expire after 5 minutes

--- background.js
+++ background.js
@@ -1,11 +1,25 @@
 const { runtime } = chrome;
+const timeMultiplier = 10; // set to 1 for testing
 let permittedHostname;
 let permittedTabId;
+let timeoutId;
+
+function setPermittedFlags(hostname, tabId) {
+    permittedHostname = hostname;
+    permittedTabId = tabId;
+
+    if (!timeoutId) {
+      timeoutId = setTimeout(() => {
+        permittedHostname = null;
+        permittedTabId = null;
+        timeoutId = null;
+      }, 5 * 360000 * timeMultiplier);
+    }
+}
 
 function onMessage({ hostname, getHostname }, { tab }, sendResponse) {
   if (hostname) {
-    permittedHostname = hostname;
-    permittedTabId = tab.id;
+    setPermittedFlags(hostname, tab.id);
   } else if (getHostname) {
     // using separate if condition to allow for expanding possible messages
     if (tab.id === permittedTabId) sendResponse(permittedHostname);
@@ -13,8 +27,3 @@ function onMessage({ hostname, getHostname }, { tab }, sendResponse) {
 }
 
 runtime.onMessage.addListener(onMessage);
-
-setTimeout(() => {
-  permittedHostname = null;
-  permittedTabId = null;
-}, 5 * 3600000);
